f26c57b9317860c3586b5f0b55cd7592495ef952
Taylor Code <taylorcode.projects@gmail.com>
2023-11-24 17:17:40 -0700

    feat(exercise 1): specify breakpoints

--- src/constants.js
+++ src/constants.js
@@ -5,14 +5,22 @@ export const COLORS = {
     300: 'hsl(190deg 5% 80%)',
     500: 'hsl(196deg 4% 60%)',
     700: 'hsl(220deg 5% 40%)',
-    900: 'hsl(220deg 3% 20%)',
+    900: 'hsl(220deg 3% 20%)'
   },
   primary: 'hsl(340deg 65% 47%)',
-  secondary: 'hsl(240deg 60% 63%)',
+  secondary: 'hsl(240deg 60% 63%)'
 };
 
 export const WEIGHTS = {
   normal: 500,
   medium: 600,
-  bold: 800,
+  bold: 800
+};
+
+const PX_TO_REM = 16;
+
+export const BREAKPOINTS = {
+  mobile: 550 / PX_TO_REM, // 0-550px
+  tablet: 1100 / PX_TO_REM, //550-1100px
+  laptop: 1500 / PX_TO_REM // 1100-1500px
 };
